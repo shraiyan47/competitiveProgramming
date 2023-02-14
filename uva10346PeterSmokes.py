c7737f5ae6261b6551bfc6ed8dd0552ea78ecfea
shahadat.hossain <shahadat.hossain@leads-bd.com>
2023-02-14 15:06:19 +0600

    uva10346

---
+++ uva10346PeterSmokes.py
@@ -0,0 +1,15 @@
+while True:
+    try:
+        n,k=map(int,input().split())
+        print("n", n)
+        print("k", k)
+        result = int((k*n-1)/(k-1))
+        print(result)
+
+    except EOFError:
+        break
+
+# n = int(input())
+# k = int(input())
+# result = int((k*n-1)/(k-1))
+# print(format(result)+"\n")
